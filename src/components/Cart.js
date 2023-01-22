11766502e27f0c7c44e6efc207da52c9151760e8
Alex <keeley.lp@gmail.com>
2023-01-22 20:53:06 +0000

    Added basic styling to cart component with total and total price elements within table

--- src/components/Cart.js
+++ src/components/Cart.js
@@ -1,6 +1,9 @@
 import React from "react";
+import "./Cart.css";
 
-export default function Cart() {
+export default function Cart(items) {
+  const counts = items.cart;
+
   return (
     <div className="cart">
       <table>
@@ -9,7 +12,17 @@ export default function Cart() {
             <td colSpan="3">Items</td>
           </tr>
         </thead>
-        <tbody></tbody>
+        <tbody>
+          {Object.entries(counts).map(([itemName, count]) => (
+            <tr key={itemName}></tr>
+          ))}
+          <tr>
+            <td colSpan="2">
+              Total <p id="discount">(discounts applied)</p>
+            </td>
+            <td>Total Price</td>
+          </tr>
+        </tbody>
       </table>
     </div>
   );
