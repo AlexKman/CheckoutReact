535663bdc85414058ffc3bf3df7cf952c313835c
Alex <keeley.lp@gmail.com>
2023-01-22 19:49:10 +0000

    Added cart component, imported within app

--- src/App.js
+++ src/App.js
@@ -1,11 +1,14 @@
 import "./App.css";
 import Header from "./components/Header";
 import ItemsList from "./components/ItemsList";
+import Cart from "./components/Cart";
 
 function App() {
   return (
     <div className="App">
+      <Cart></Cart>
       <Header></Header>
+
       <ul className="items">
         <ItemsList></ItemsList>
       </ul>
