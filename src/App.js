19ed4903e8a5da9c623b6d9f33c5b1abc81f4314
Alex <keeley.lp@gmail.com>
2023-01-22 17:37:38 +0000

    Added images to display for cart

--- src/App.js
+++ src/App.js
@@ -1,10 +1,20 @@
 import "./App.css";
 import Header from "./components/Header";
+import apple from "./images/apple.png";
 
 function App() {
+  const items = {
+    A: {
+      src: { apple },
+      price: 50,
+      special: [3, 130],
+    },
+  };
+
   return (
     <div className="App">
       <Header></Header>
+      <ul id="items">{items}</ul>
     </div>
   );
 }
